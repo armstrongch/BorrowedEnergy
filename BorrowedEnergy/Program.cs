2e7146f714cfd6bebcd29629c8dc1b2b32c40c91
Turd Boomerang <christopherernestarmstrong@gmail.com>
2023-03-22 08:12:42 -0400

    Add project files.

---
+++ BorrowedEnergy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorrowedEnergy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BorrowedEnergy/Runner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorrowedEnergy
+{
+    internal class Runner
+    {
+        public int max_energy { get; private set; }
+        public int daily_energy_recharge { get; private set; } = 10;
+
+    }
+}
